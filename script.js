18809179f8aa54694fd08d6530e57bc3e44a4a1f
Kamrun <kamrun233420@gmail.com>
2024-08-30 00:20:08 +0600

    updated

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ let msg2 = document.querySelector(".msg2");
 let form = document.querySelector(".form");
 let form2 = document.querySelector(".form2");
 let form3 = document.querySelector(".form3");
+let testCmnt1 = document.querySelector("#test-cmnt1");
+let testCmnt2 = document.querySelector("#test-cmnt2");
+let arr1 = document.querySelector(".arr1");
+let arr2 = document.querySelector(".arr2");
 
 seeMoreBtn.addEventListener("click", ()=>{
     products2.classList.remove("hide");
@@ -41,3 +45,13 @@ function showmsg(event){
 //     msg.classList.remove("hide");
 //     form.addEventListener.add("hide");
 // }).preventDefault();
+
+arr2.addEventListener("click", ()=>{
+    testCmnt2.classList.remove("hide");
+    testCmnt1.classList.add("hide");
+})
+
+arr1.addEventListener("click", ()=>{
+    testCmnt1.classList.remove("hide");
+    testCmnt2.classList.add("hide");
+})
